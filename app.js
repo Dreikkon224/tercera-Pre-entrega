099bfed9b0520e17af77ae52a72173ec8588831d
Benjamin <alejandrogm224@gmail.com>
2023-05-02 22:43:07 -0300

    commit final

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-const productos = [new Producto(1,"naruto","ShonenJump",4,2,500),new Producto(2,"OnePiece","ShonenJump",3,1,400),new Producto(3,"ReZero","PaniniManga",2,4,224)]
+let carrito = []
+
+let objNaruto = new Producto(1,"Naruto","ShonenJump",4,2,500);
+let objOnePiece = new Producto(2,"OnePiece","ShonenJump",3,1,400);
+let objReZerp = new Producto(3,"ReZero","PaniniManga",2,4,224);
+
+const productos = [objNaruto,objOnePiece,objReZerp]
+
+document.addEventListener('DOMContentLoaded', () => {
+  const carritoLocal = JSON.parse(localStorage.getItem('carrito'));
+  if (carritoLocal != null)
+  {
+    carrito = carritoLocal;
+  }
+  mostrarCarrito();
+});
 
 const boton1 = document.getElementById("boton1");
 const boton2 = document.getElementById("boton2");
@@ -7,58 +22,58 @@ const boton3 = document.getElementById("boton3");
 
 boton1.addEventListener('click', () => {
     const producto = productos.find ((item) =>{
-        return item.id === boton1.dataset.id
+        return item.id === +boton1.dataset.id
     });
     carrito.push(producto);
+    localStorage.setItem('carrito',JSON.stringify(carrito));
     mostrarCarrito();
 })
 
 boton2.addEventListener('click', () => {
     const producto = productos.find ((item) =>{
-        return item.id === boton2.dataset.id
+        return item.id === +boton2.dataset.id
     });
     carrito.push(producto);
+    localStorage.setItem('carrito',JSON.stringify(carrito));
     mostrarCarrito();
 })
 
 boton3.addEventListener('click', () => {
     const producto = productos.find ((item) =>{
-        return item.id === boton3.dataset.id
+        return item.id === +boton3.dataset.id
     });
     carrito.push(producto);
+    localStorage.setItem('carrito',JSON.stringify(carrito));
     mostrarCarrito();
 })
 
 function mostrarCarrito() {
-    const tabla = document.getElementById('producto');
+    const tabla = document.getElementById('items');
     tabla.innerHTML += ``;
     let counter = 1;
 
-    carrito.array.forEach((producto) => {
+    carrito.forEach((producto) => {
         tabla.innerHTML +=`
             <tr>
-                <th>${counter}<th>
-                <th>${producto.nombre}<th>
-                <th>${producto.editorial}<th>
-                <th>${producto.volumen}<th>
-                <th>${producto.cantidad}<th>
-                <th>${producto.precio}<th>
-            <tr>
+                <td>${counter}</td>
+                <td>${producto.nombre}</td>
+                <td>${producto.editorial}</td>
+                <td>${producto.volumen}</td>
+                <td>${producto.cantidad}</td>
+                <td>${producto.precio}</td>
+            </tr>
         `;
         counter++;
     });
     tr = document.createElement('tr');
     tr.innerHTML = `<th><th>
-                        <td><td>
-                        <td><td>
-                        <td><td>
-                        <td><td>
-                        <td><td>
-                        <td>${carrito.reduce((total,item) => total + item-precio,0)} <td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>${carrito.reduce((total,item) => total + item.precio,0)} </td>
                         `;
         tabla.appendChild(tr);
 
 }
-
-cargarItems();
-mostrarCarrito();
--- items.js
+++ items.js
@@ -1,13 +1,19 @@
-class Item{
+class Producto{
     ////Atributos
-    manga;
+    nombre;
     editorial;
     volumen;
-    catidad;
+    cantidad;
     precio;
 
-
-//// Metodo y Acción
+constructor(id,nombre,editorial,volumen,cantidad,precio){
+    this.id = id
+    this.nombre = nombre
+    this.editorial = editorial
+    this.volumen = volumen
+    this.cantidad = cantidad
+    this.precio = precio
+}
 
     subTotal = () => {
         return this.precio*this.cantidad;
